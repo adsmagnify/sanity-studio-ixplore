22de97fbc780f48a91a5a76fa95650b734f191a7
adsmagnify <adsmagnify@gmail.com>
2025-12-23 14:57:36 +0530

    updated ui/ux

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,14 +1,17 @@
-import {defineConfig} from 'sanity'
+import {defineConfig, buildLegacyTheme} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
 import {structure} from './structure'
 import {themePlugin} from './plugins/themePlugin'
+import {theme} from 'https://themer.sanity.build/api/hues?preset=verdant&default=5c9199;950;lightest:e5fff1'
+
+
 
 export default defineConfig({
   name: 'default',
   title: 'Ixplore',
-
+  theme,
   projectId: 'r8vvgwgu',
   dataset: 'production',
 
@@ -24,3 +27,5 @@ export default defineConfig({
     types: schemaTypes,
   },
 })
+
+
